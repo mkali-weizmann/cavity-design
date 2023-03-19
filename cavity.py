810209bd6d1f9f4e3bcb5b1ea09118c8f5f10ec1
mkali-weizmann <michael.kali@weizmann.ac.il>
2023-03-19 14:53:17 +0200

    This is an analytical approach to ray tracing. I am giving it up because the calculation for the last ray is 8 seconds long (or 42 seconds if I don't simplify it in the middle), and so it seems to be not computationally efficient, and moreover - the expressions are too complicated to carry any meaningful information

--- cavity.py
+++ cavity.py
@@ -1,30 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 import sympy as sp
+import sympy.vector as spv
 
+S = spv.CoordSys3D('S')
 
 class Ray:
-    def __init__(self, x: sp.Symbol, y: sp.Symbol, theta: sp.Symbol):
-        self.x = x
-        self.y = y
-        self.theta = theta
-
-    @property
-    def ax_b(self) -> Tuple[sp.tan, sp.Symbol]:
-        a = sp.tan(self.theta)
-        b = self.y - a * self.x
-        return a, b
+    def __init__(self, origin: spv.Vector, direction: spv.Vector, length: Optional[sp.Symbol] = None):
+        self.origin = origin
+        self.direction = direction.normalize()
+        self.length = length
 
     def plot(self, substitution_dict: dict):
-        a, b = self.ax_b
-        a_subs = a.subs(substitution_dict)
-        b_subs = b.subs(substitution_dict)
-        # x_plot = sp.Symbol('x_plot')
-        # sp.plot(a_subs * x_plot + b_subs, (x_plot, 0, 1))
-        x_plot = np.linspace(0, 1, 100)
-        y_plot = a_subs * x_plot + b_subs
-        plt.plot(x_plot, y_plot)
+        origin_x_plot = self.origin.coeff(S.i).subs(substitution_dict).evalf()
+        origin_y_plot = self.origin.coeff(S.j).subs(substitution_dict).evalf()
+        direction_x_plot = self.direction.coeff(S.i).subs(substitution_dict).evalf()
+        direction_y_plot = self.direction.coeff(S.j).subs(substitution_dict).evalf()
+        if self.length is None:
+            length_plot = 1
+        else:
+            length_plot = self.length.subs(substitution_dict).evalf()
+        x = np.array([origin_x_plot, origin_x_plot + length_plot * direction_x_plot])
+        y = np.array([origin_y_plot, origin_y_plot + length_plot * direction_y_plot])
+        plt.plot(x, y, 'r-')
 
 
 class Mirror:
@@ -33,35 +32,42 @@ class Mirror:
 
 
 class FlatMirror(Mirror):
-    def __init__(self, x: sp.Symbol, y: sp.Symbol, theta: sp.Symbol):
-        self.x = x
-        self.y = y
-        self.theta = theta  # Direction of the normal to the mirror
+    def __init__(self, origin: spv.Vector, normal_direction: spv.Vector):
+        self.origin = origin
+        self.normal_direction = normal_direction.normalize()
 
     @property
-    def ax_b(self) -> Tuple[sp.Symbol, sp.Symbol]:
-        a = - 1 / sp.tan(self.theta)
-        b = self.y - a * self.x
-        return a, b
+    def mirror_line_direction(self):
+        return self.normal_direction.cross(S.k)
 
-    def deflect_ray(self, ray:Ray) -> Ray:
-        mirror_a, mirror_b = self.ax_b
-        ray_a, ray_b = ray.ax_b
-        x_incidence = (mirror_b - ray_b) / (ray_a - mirror_a)
-        y_incidence = mirror_a * x_incidence + mirror_b
-        theta = 2 * self.theta - ray.theta - sp.pi
-        return Ray(x_incidence, y_incidence, theta)
+    def find_intersection_with_ray(self, ray: Ray) -> spv.Vector:
+        t, s = sp.symbols('t s')
+        x_ray = ray.origin.coeff(S.i) + ray.direction.coeff(S.i) * t
+        y_ray = ray.origin.coeff(S.j) + ray.direction.coeff(S.j) * t
+        x_mirror = self.origin.coeff(S.i) + self.mirror_line_direction.coeff(S.i) * s
+        y_mirror = self.origin.coeff(S.j) + self.mirror_line_direction.coeff(S.j) * s
+        s_and_t = sp.solve([sp.simplify(x_ray - x_mirror), sp.simplify(y_ray - y_mirror)], [t, s])
+        return x_ray.subs(t, s_and_t[t]) * S.i + y_ray.subs(t, s_and_t[t]) * S.j
+
+    def reflect_along_normal(self, ray: Ray) -> spv.Vector:
+        normal = self.normal_direction.normalize()
+        return ray.direction - 2 * normal.dot(ray.direction) * normal
+
+    def deflect_ray(self, ray: Ray) -> Ray:
+        intersection = self.find_intersection_with_ray(ray)
+        reflected_direction = self.reflect_along_normal(ray)
+        ray.length = (intersection - ray.origin).magnitude()
+        return Ray(intersection, reflected_direction)
 
     def plot(self, substitution_dict: dict):
-        a, b = self.ax_b
-        a_subs = a.subs(substitution_dict).evalf()
-        b_subs = b.subs(substitution_dict).evalf()
-        # x_plot = sp.Symbol('x_plot')
-        # sp.plot(a_subs * x_plot + b_subs, (x_plot, -1, 1))
-        x = np.linspace(-1, 1, 100)
-        y = a_subs * x + b_subs
-        plt.plot(x, y, 'k-')
-        plt.axis('equal')
+        origin_x_plot = self.origin.coeff(S.i).subs(substitution_dict).evalf()
+        origin_y_plot = self.origin.coeff(S.j).subs(substitution_dict).evalf()
+        direction_x_plot = self.mirror_line_direction.coeff(S.i).subs(substitution_dict).evalf()
+        direction_y_plot = self.mirror_line_direction.coeff(S.j).subs(substitution_dict).evalf()
+        length_plot = 1
+        x = np.array([origin_x_plot - length_plot * direction_x_plot, origin_x_plot + length_plot * direction_x_plot])
+        y = np.array([origin_y_plot - length_plot * direction_y_plot, origin_y_plot + length_plot * direction_y_plot])
+        plt.plot(x, y, 'b-')
 
 
 class Cavity:
@@ -84,5 +90,22 @@ class Cavity:
         else:
             return self.ray_history[-1]
 
-    # def solve(self, ray: Ray) -> Ray:
-    #     x_output, y_output, theta_output = sp.solve([])
+# %%
+# import time
+# start_time = time.time()
+# x_1, y_1, direction_x_1, direction_y_1, x_2, y_2, direction_x_2, direction_y_2, x_3, y_3, direction_x_3, direction_y_3, direction_x_ray, direction_y_ray = sp.symbols('x_1, y_1, direction_x_1, direction_y_1, x_2, y_2, direction_x_2, direction_y_2, x_3, y_3, direction_x_3, direction_y_3, direction_x_ray, direction_y_ray')
+# origin_1 = x_1 * S.i + y_1 * S.j
+# origin_2 = x_2 * S.i + y_2 * S.j
+# origin_3 = x_3 * S.i + y_3 * S.j
+# direction_1 = direction_x_1 * S.i + direction_y_1 * S.j
+# direction_2 = direction_x_2 * S.i + direction_y_2 * S.j
+# direction_3 = direction_x_3 * S.i + direction_y_3 * S.j
+# direction_ray = direction_x_ray * S.i + direction_y_ray * S.j
+# initial_ray = Ray(origin_1, direction_ray)
+# mirror1 = FlatMirror(origin_1, direction_1)
+# mirror2 = FlatMirror(origin_2, direction_2)
+# mirror3 = FlatMirror(origin_3, direction_3)
+# deflected_ray2 = mirror2.deflect_ray(initial_ray)
+# deflected_ray3 = mirror3.deflect_ray(deflected_ray2)
+# time_duration = time.time() - start_time
+# print(f'Calculation took {time_duration} seconds')
