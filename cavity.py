Fix jacobian mistake

cavity.py
@@ -2474,7 +2474,7 @@ class OpticalSystem:
         if given_initial_central_line is not None:
             if isinstance(given_initial_central_line, Ray):
                 self.set_given_central_line(initial_ray=given_initial_central_line)
-            elif given_initial_central_line is True:
+            elif given_initial_central_line is True and len(self.arms) > 0:
                 self.set_given_central_line(initial_ray=self.default_initial_ray)
         if given_initial_local_mode_parameters is not None:
             self.set_given_mode_parameters(
@@ -5511,7 +5511,10 @@ def optical_system_to_cavity_completion(
     if len(optical_system.surfaces) < 2:
         optical_axis = optical_system.surfaces[0].inwards_normal
     else:
-        optical_axis = normalize_vector(optical_system.surfaces[-1].center - optical_system.surfaces[-2].center)
+        if optical_system.central_line is None:
+            optical_system.set_given_central_line(initial_ray=optical_system.default_initial_ray)
+        output_ray = optical_system.surfaces[-1].propagate_ray(optical_system.central_line[-1])
+        optical_axis = output_ray.k_vector
     if (
         NA is not None
     ):  # Generate the appropriate mode, propagate it through the system, and then match the end mirror to the output mode.
@@ -5528,7 +5531,7 @@ def optical_system_to_cavity_completion(
             mode_parameters=mode_parameters_first_arm, propagate_with_first_surface_first=False
         )[-1]
         output_mode = output_mode_local.to_mode_parameters(
-            location_of_local_mode_parameter=optical_system.arms[-1].surface_1.center, k_vector=optical_axis
+            location_of_local_mode_parameter=optical_system.surfaces[-1].center, k_vector=optical_axis
         )
         if end_mirror_ROC is not None and end_mirror_distance_to_last_element is not None:  # Overrides the NA
             end_mirror = CurvedMirror(
@@ -5539,7 +5542,7 @@ def optical_system_to_cavity_completion(
             )
         elif end_mirror_distance_to_last_element is not None and end_mirror_ROC is None:  # Fixed NA and last arm length
             z_minus_z_0 = (
-                np.linalg.norm(output_mode.center[0, :] - last_surface.center) + end_mirror_distance_to_last_element
+                (last_surface.center - output_mode.center[0, :]) @ optical_axis + end_mirror_distance_to_last_element
             )
             end_mirror = match_a_mirror_to_mode(mode=output_mode, z=z_minus_z_0, name="End mirror", **end_mirror_kwargs)
         elif end_mirror_ROC is not None and end_mirror_distance_to_last_element is None:  # Fixed NA and last mirror ROC

simple_analysis_scripts/potential_analysis/analyze_potential.py
@@ -1038,7 +1038,6 @@ def hessian_ray_tracing(cavity: Cavity, n_rays: int = 30, phi_max: float = 0.02)
     return hessian
 
 def hessian_ABCD_matrices(cavity: Cavity, n_rays: int = 30, phi_max: float = 0.02):
-
     end_points, end_directions_inverted, optical_system_inverted_reduced = orthonormal_rays_end_points(cavity=cavity,
                                                                                                        n_rays=n_rays,
                                                                                                        phi_max=phi_max)
@@ -1053,7 +1052,7 @@ def hessian_ABCD_matrices(cavity: Cavity, n_rays: int = 30, phi_max: float = 0.0
     hessian = -(output_ROC - optical_system_inverted_reduced.surfaces[-1].radius) / (output_ROC * optical_system_inverted_reduced.surfaces[-1].radius)
     return hessian
 
-def energy_level(cavity: Cavity, hessian_method: str = 'ray_tracing'):
+def energy_level(cavity: Cavity, hessian_method: str = 'ABCD_matrices'):
     # Corresponds to equations  eq: potential scaling - fixed mode width and eq:potential scaling - general (commented out here) in my notes.
     if hessian_method == 'ray_tracing':
         hessian = hessian_ray_tracing(cavity=cavity, n_rays = 1)[0, 0]
@@ -1063,13 +1062,14 @@ def energy_level(cavity: Cavity, hessian_method: str = 'ray_tracing'):
         raise ValueError(f'Invalid hessian method: {hessian_method}')
     # The energy level of the mode is proportional to the square root of the product of the two eigenvalues of the Hessian matrix.
     spot_size_end = cavity.arms[len(cavity.arms) // 2].mode_parameters_on_surface_0.spot_size[0]
-    # results_dict = analyze_potential_given_cavity(cavity=cavity, n_rays = 10, phi_max = 0.01, print_tests=False)
-    # potential_quadratic_coefficient = results_dict['polynomial_residuals_mirror'].coef[1]  # it is a polynomial of x**2, so quadratic term is the second term in the array
+    results_dict = analyze_potential_given_cavity(cavity=cavity, n_rays = 10, phi_max = 0.01, print_tests=False)
+    potential_quadratic_coefficient = results_dict['polynomial_residuals_mirror'].coef[1]  # it is a polynomial of x**2, so quadratic term is the second term in the array
     jacobian = mirrors_jacobian(cavity=cavity)
     # potential_quadratic_coefficient_normalized = potential_quadratic_coefficient * jacobian
-    hessian_normalized = hessian * jacobian
-    # energy_level_hessian_and_potential = np.sqrt(potential_quadratic_coefficient / (-2 * hessian)) * cavity.lambda_0_laser / np.pi
-    energy_level_hessian_only = -cavity.lambda_0_laser ** 2 / (2 * np.pi ** 2 * spot_size_end ** 2 * hessian_normalized)
+    hessian_normalized = hessian * jacobian**2
+    energy_level_hessian_only = cavity.lambda_0_laser ** 2 / (2 * np.pi ** 2 * spot_size_end**2 * hessian_normalized)
+    energy_level_hessian_and_potential = np.sqrt(potential_quadratic_coefficient / (-2 * hessian_normalized)) * cavity.lambda_0_laser / np.pi
+    energy_level_spot_size_and_potential = potential_quadratic_coefficient * spot_size_end**2
     return energy_level_hessian_only  # , energy_level_hessian_and_potential
 
 def mirrors_jacobian(cavity: Cavity):

simple_analysis_scripts/potential_analysis/hessian_calculation.py
@@ -1,6 +1,7 @@
 from matplotlib import use
 use('TkAgg')
 from simple_analysis_scripts.potential_analysis.analyze_potential import *
+
 # %% compare one lens and two lenses system energy level for the same spot_size:
 params = [
           OpticalElementParams(name='LaserOptik mirror'      ,surface_type='curved_mirror'                  , x=-5e-03                  , y=0                       , z=0                       , theta=0                       , phi=1e+00 * np.pi           , r_1=5e-03                   , r_2=np.nan                  , curvature_sign=CurvatureSigns.concave, T_c=np.nan                  , n_inside_or_after=1e+00                   , n_outside_or_before=1e+00                   , diameter=7.75e-03                , material_properties=MaterialProperties(refractive_index=1.45e+00                , alpha_expansion=5.2e-07                 , beta_surface_absorption=1e-06                   , kappa_conductivity=1.38e+00                , dn_dT=1.2e-05                 , nu_poisson_ratio=1.6e-01                 , alpha_volume_absorption=1e-03                   , intensity_reflectivity=1e-04                   , intensity_transmittance=9.99899e-01             , temperature=np.nan                  ), polynomial_coefficients=None),
@@ -19,16 +20,47 @@ optical_system_two_lenses = OpticalSystem.from_params(params[:-1], lambda_0_lase
 
 cavity_single_lens = optical_system_to_cavity_completion(optical_system=optical_system_single_lens,
                                                          NA=cavity_two_lenses.arms[0].mode_parameters.NA[0],
-                                                         end_mirror_distance_to_last_element=params[-2].x - params[-3].x)
+                                                         end_mirror_distance_to_last_element=cavity_two_lenses.surfaces[3].center[0] - cavity_two_lenses.surfaces[2].center[0])
 
-energy_level_single_lens = energy_level(cavity=cavity_single_lens, hessian_method = 'ABCD_matrices')
-energy_level_two_lenses = energy_level(cavity=cavity_two_lenses, hessian_method = 'ABCD_matrices')
+optical_system_mirror_right = OpticalSystem(surfaces=[cavity_single_lens.surfaces[-1]], lambda_0_laser=LAMBDA_0_LASER, use_paraxial_ray_tracing=False, p_is_trivial=True, t_is_trivial=True)
+cavity_fabry_perot = optical_system_to_cavity_completion(optical_system=optical_system_mirror_right,
+                                                         NA=cavity_two_lenses.arms[2].mode_parameters.NA[0],
+                                                         end_mirror_distance_to_last_element=cavity_two_lenses.surfaces[3].center[0] - cavity_two_lenses.surfaces[2].center[0])
 
-print(f'Energy level single lens: {energy_level_single_lens} m')
-print(f'Energy level cavity_original: {energy_level_two_lenses} m')
+cavity_single_lens_shortened = optical_system_to_cavity_completion(optical_system=optical_system_single_lens,
+                                                 NA=cavity_two_lenses.arms[0].mode_parameters.NA[0],
+                                                 end_mirror_distance_to_last_element=cavity_two_lenses.surfaces[3].center[0] - cavity_two_lenses.surfaces[2].center[0] - 0.1)
+params_symmetric_long_arm = [
+          OpticalElementParams(name='LaserOptik mirror'      ,surface_type='curved_mirror'                  , x=-5e-03                  , y=0                       , z=0                       , theta=0                       , phi=1e+00 * np.pi           , r_1=5e-03                   , r_2=np.nan                  , curvature_sign=CurvatureSigns.concave, T_c=np.nan                  , n_inside_or_after=1e+00                   , n_outside_or_before=1e+00                   , diameter=7.75e-03                , material_properties=MaterialProperties(refractive_index=1.45e+00                , alpha_expansion=5.2e-07                 , beta_surface_absorption=1e-06                   , kappa_conductivity=1.38e+00                , dn_dT=1.2e-05                 , nu_poisson_ratio=1.6e-01                 , alpha_volume_absorption=1e-03                   , intensity_reflectivity=1e-04                   , intensity_transmittance=9.99899e-01             , temperature=np.nan                  ), polynomial_coefficients=None),
+          OpticalElementParams(name='spherical_lens'         ,surface_type='thick_lens'                     , x=6.387481683531355e-03   , y=0                       , z=0                       , theta=0                       , phi=0                       , r_1=2.422e-02               , r_2=-5.488e-03              , curvature_sign=CurvatureSigns.convex, T_c=2.913797540986543e-03   , n_inside_or_after=1.76e+00                , n_outside_or_before=1e+00                   , diameter=7.75e-03                , material_properties=MaterialProperties(refractive_index=1.76e+00                , alpha_expansion=5.5e-06                 , beta_surface_absorption=1e-06                   , kappa_conductivity=4.606e+01               , dn_dT=1.17e-05                , nu_poisson_ratio=3e-01                   , alpha_volume_absorption=1e-02                   , intensity_reflectivity=1e-04                   , intensity_transmittance=9.99899e-01             , temperature=np.nan                  ), polynomial_coefficients=None),
+          OpticalElementParams(name='Negative Lens'          ,surface_type='thick_lens'                     , x=4.786693804540247e-01   , y=0                       , z=0                       , theta=0                       , phi=0                       , r_1=-6.059380397196618e-02  , r_2=4.761904761904711e-01   , curvature_sign=CurvatureSigns.concave, T_c=3.45e-03                , n_inside_or_after=1.45e+00                , n_outside_or_before=1e+00                   , diameter=5e-02                   , material_properties=MaterialProperties(refractive_index=1.45e+00                , alpha_expansion=5.2e-07                 , beta_surface_absorption=1e-06                   , kappa_conductivity=1.38e+00                , dn_dT=1.2e-05                 , nu_poisson_ratio=1.6e-01                 , alpha_volume_absorption=1e-03                   , intensity_reflectivity=1e-04                   , intensity_transmittance=9.99899e-01             , temperature=np.nan                  ), polynomial_coefficients=None),
+          OpticalElementParams(name='Surface_3'              ,surface_type='curved_mirror'                  , x=4.860510834981394e-01   , y=0                       , z=0                       , theta=0                       , phi=0                       , r_1=9.040000000000001e-02   , r_2=np.nan                  , curvature_sign=CurvatureSigns.concave, T_c=np.nan                  , n_inside_or_after=1e+00                   , n_outside_or_before=1e+00                   , diameter=5e-02                   , material_properties=MaterialProperties(refractive_index=1.45e+00                , alpha_expansion=5.2e-07                 , beta_surface_absorption=1e-06                   , kappa_conductivity=1.38e+00                , dn_dT=1.2e-05                 , nu_poisson_ratio=1.6e-01                 , alpha_volume_absorption=1e-03                   , intensity_reflectivity=1e-04                   , intensity_transmittance=9.99899e-01             , temperature=np.nan                  ), polynomial_coefficients=None)]
+cavity_symmetric_right_arm = Cavity.from_params(params=params_symmetric_long_arm, lambda_0_laser=LAMBDA_0_LASER, use_paraxial_ray_tracing=False, p_is_trivial=True, t_is_trivial=True)
+
+
+energy_level_single_lens = energy_level(cavity=cavity_single_lens)
+energy_level_two_lenses = energy_level(cavity=cavity_two_lenses)
+energy_level_fabry_perot = energy_level(cavity=cavity_fabry_perot)
+energy_level_single_lens_shortened = energy_level(cavity=cavity_single_lens_shortened)
+energy_level_symmetric_right_arm = energy_level(cavity=cavity_symmetric_right_arm)
+
+print(f'V(w) cavity_original: {energy_level_two_lenses} m')
+print(f'V(w) single lens: {energy_level_single_lens} m')
+print(f'V(w) fabry perot: {energy_level_fabry_perot} m')
+print(f'V(w) single lens shortened: {energy_level_single_lens_shortened} m')
+print(f'V(w) symmetric right arm: {energy_level_symmetric_right_arm} m')
 # %%
 cavity_single_lens.plot()
 plt.show()
 
 cavity_two_lenses.plot()
+plt.show()
+
+cavity_fabry_perot.plot()
+plt.show()
+
+cavity_single_lens_shortened.plot()
+plt.show()
+
+cavity_symmetric_right_arm.plot()
 plt.show()

simple_analysis_scripts/small_debugging_scripts/compare_hessian_potential_spot_size_to_analytical.py
@@ -81,16 +81,17 @@ results_dict = analyze_potential_given_cavity(cavity=cavity, n_rays = 10, phi_ma
 a_2_numerical = results_dict['polynomial_residuals_mirror'].coef[1]
 a_2_analytical = u / (2 * R_1 ** 2)
 
-hessian_normalized = hessian_ABCD_matrices_value * jacobian
-a_2_normalized = a_2_analytical * jacobian
+hessian_normalized = hessian_ABCD_matrices_value * jacobian ** 2
+# a_2_normalized = a_2_analytical * jacobian
 
 w_squared_numerical = cavity.arms[0].mode_parameters_on_surface_1.spot_size[0] ** 2
 w_squared_analytical_potential = cavity.lambda_0_laser / (np.pi * np.sqrt(-2 * hessian_ABCD_matrices_value * a_2_analytical))
-w_squared_analytical_potential_normalized = cavity.lambda_0_laser / (np.pi * np.sqrt(-2 * hessian_normalized * a_2_normalized))
+w_squared_analytical_potential_normalized = cavity.lambda_0_laser / (np.pi * np.sqrt(-2 * hessian_normalized * a_2_analytical))
 # w_squared_analytical_optics = R * cavity.lambda_0_laser / np.pi * np.sqrt(2 * R / u)
 
-energy_level_hessian_and_potential = np.sqrt(a_2_numerical / (-2 * hessian_analytical)) * cavity.lambda_0_laser / np.pi
-energy_level_hessian_only = cavity.lambda_0_laser ** 2 / (2 * np.pi ** 2 * w_squared_analytical_potential_normalized * hessian_normalized)
+energy_level_hessian_only = cavity.lambda_0_laser ** 2 / (2 * np.pi ** 2 * w_squared_numerical * hessian_normalized)
+energy_level_hessian_and_potential = np.sqrt(a_2_analytical / (-2 * hessian_normalized)) * cavity.lambda_0_laser / np.pi
+energy_level_spot_size_and_potential = a_2_analytical * w_squared_numerical
 
 # plot_results(results_dict)
 # plt.show()
@@ -105,5 +106,6 @@ print(f'Analytical spot size potential squared normalized: {w_squared_analytical
 print(f'Spot sizes squared ratio: {w_squared_numerical / w_squared_analytical_potential:.5f}')
 print(f'Energy level from Hessian only: {energy_level_hessian_only:.3e} m')
 print(f'Energy level from Hessian and potential: {energy_level_hessian_and_potential:.3e} m')
+print(f'Energy level from spot size and potential: {energy_level_spot_size_and_potential:.3e} m')
 
 energy_level_ABCD_matrices_value = energy_level(cavity=cavity, hessian_method = 'ABCD_matrices')