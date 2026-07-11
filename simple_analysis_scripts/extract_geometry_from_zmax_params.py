b7b5a30f937571c9850d7caedb7b31f32e5c4522
michaeka <michael.kali@weizmann.ac.il>
2026-07-11 20:37:19 +0300

    Working zmax file extraction process (the problem was me ignoring the required BK7 window)

--- simple_analysis_scripts/extract_geometry_from_zmax_params.py
+++ simple_analysis_scripts/extract_geometry_from_zmax_params.py
@@ -1,6 +1,7 @@
 from matplotlib import use
 use("TkAgg")
 import sympy as sp
+import numpy as np
 
 def get_taylor_numpy_array(max_power, numeric_params=None):
     """
@@ -93,6 +94,8 @@ print("Numeric NumPy Array:")
 print("-" * 55)
 print(repr(num_array))
 
+## ATTENTION: the output polynomial here should have it's sign inverse because ZMAX uses the sign convention, and
+# my polynomial representation is indifferent to the direction of the beam, so it always has positive curvature.
 num_array = get_taylor_numpy_array(20, numeric_params=surf2_params)
 print("Numeric NumPy Array:")
 print("-" * 55)
@@ -101,22 +104,25 @@ print(repr(num_array))
 # %%
 from cavity_design import *
 
-params_edmunds = [OpticalSurfaceParams(name='aspheric_lens_left',  surface_type='aspheric_surface' , x=0   , y=0                       , z=0                       , theta=0                       , phi=-1e+00 * np.pi          , radius=3.817155986760137343E-01    , curvature_sign=CurvatureSigns.convex, T_c=3.1/2e-3                  , n_inside_or_after=1.583e+00               , n_outside_or_before=1e+00                   , diameter=6.3e-03                 , material_properties=MaterialProperties(refractive_index=None                    , alpha_expansion=None                    , beta_surface_absorption=None                    , kappa_conductivity=None                    , dn_dT=None                    , nu_poisson_ratio=None                    , alpha_volume_absorption=None                    , intensity_reflectivity=None                    , intensity_transmittance=None                    , temperature=np.nan                  ), polynomial_coefficients=-np.array([ 0.00000000e+00, -3.94470149e+01,  4.40171309e+06, -9.01563757e+10, -1.60183137e+15, -3.23783960e+15, -1.51148859e+19, -7.39192926e+22, -3.73825954e+26, -1.93899408e+30, -1.02584959e+34])),
-                  OpticalSurfaceParams(name='aspheric_lens_right', surface_type='aspheric_surface' , x=3.1e-3   , y=0                       , z=0                       , theta=0                       , phi=0                  , radius=-7.889402975752558833E-02   , curvature_sign=CurvatureSigns.concave, T_c=3.1/2e-3                  , n_inside_or_after=1e+00                   , n_outside_or_before=1.583e+00               , diameter=6.3e-03                 , material_properties=MaterialProperties(refractive_index=None                    , alpha_expansion=None                    , beta_surface_absorption=None                    , kappa_conductivity=None                    , dn_dT=None                    , nu_poisson_ratio=None                    , alpha_volume_absorption=None                    , intensity_reflectivity=None                    , intensity_transmittance=None                    , temperature=np.nan                  ), polynomial_coefficients=np.array([0.00000000e+00, 1.90857799e+02, 3.33213215e+06, 1.08218014e+11, 4.32910754e+15 , 6.27836273e+20, 2.70839583e+25, 1.72102573e+30, 1.13089247e+35, 7.62167883e+39, 5.23938302e+44]))
+params_edmunds = [OpticalSurfaceParams(name='aspheric_lens_left', surface_type='aspheric_surface' , x=0   , y=0                       , z=0                       , theta=0                       , phi=-1e+00 * np.pi                  , radius=np.nan   , curvature_sign=CurvatureSigns.convex, T_c=3.1/2e-3                  , n_inside_or_after=1.583e+00           , n_outside_or_before=1e+00      , diameter=6.3e-03                 , material_properties=MaterialProperties(refractive_index=None                    , alpha_expansion=None                    , beta_surface_absorption=None                    , kappa_conductivity=None                    , dn_dT=None                    , nu_poisson_ratio=None                    , alpha_volume_absorption=None                    , intensity_reflectivity=None                    , intensity_transmittance=None                    , temperature=np.nan                  ), polynomial_coefficients=np.array([0.00000000e+00, 1.90857799e+02, 3.33213215e+06, 1.08218014e+11, 4.32910754e+15 , 6.27836273e+20, 2.70839583e+25, 1.72102573e+30, 1.13089247e+35, 7.62167883e+39, 5.23938302e+44])),
+                  OpticalSurfaceParams(name='aspheric_lens_right',  surface_type='aspheric_surface' , x=3.1e-3   , y=0                , z=0                       , theta=0                       , phi=0                               , radius=np.nan    , curvature_sign=CurvatureSigns.concave, T_c=3.1/2e-3               , n_inside_or_after=1e+00               , n_outside_or_before=1.583e+00  , diameter=6.3e-03                 , material_properties=MaterialProperties(refractive_index=None                    , alpha_expansion=None                    , beta_surface_absorption=None                    , kappa_conductivity=None                    , dn_dT=None                    , nu_poisson_ratio=None                    , alpha_volume_absorption=None                    , intensity_reflectivity=None                    , intensity_transmittance=None                    , temperature=np.nan                  ), polynomial_coefficients=-np.array([ 0.00000000e+00, -3.94470149e+01,  4.40171309e+06, -9.01563757e+10, -1.60183137e+15, -3.23783960e+15, -1.51148859e+19, -7.39192926e+22, -3.73825954e+26, -1.93899408e+30, -1.02584959e+34])),
+                  OpticalSurfaceParams(name='BK7 window', surface_type=SurfacesTypes.flat_refractive_surface, x = 3.1e-3 + 1.483e-3, y=0, z=0, theta=0, phi=0, radius=np.inf, curvature_sign=CurvatureSigns.flat, T_c=np.nan, n_outside_or_before=1, n_inside_or_after=1.5135, material_properties=MaterialProperties(), diameter=6.3e-3, polynomial_coefficients=None)
                  ]
 
 optical_system_edmunds = OpticalSystem.from_params(params=params_edmunds, t_is_trivial=True, p_is_trivial=True, use_paraxial_ray_tracing=False)
 # %%
+back_focal_length = back_focal_length_of_lens(R_1 = optical_system_edmunds[1].radius, R_2 = -optical_system_edmunds[0].radius, n=optical_system_edmunds[0].n_2, T_c = optical_system_edmunds[1].center[0] - optical_system_edmunds[0].center[0])
 initial_ys = np.linspace(0, 0.0015, 10)
-initial_xs = np.ones_like(initial_ys) * 0.01
+initial_xs = np.ones_like(initial_ys) * (-0.01)
 initial_positions = np.stack([initial_xs, initial_ys, np.zeros_like(initial_ys)], axis=-1)
-ray_0 = Ray(origin=initial_positions, k_vector=LEFT, n=1)
+ray_0 = Ray(origin=initial_positions, k_vector=RIGHT, n=1)
 
-optical_system_edmunds_inverted = optical_system_edmunds.invert()
+# optical_system_edmunds_inverted = optical_system_edmunds.invert()
 
-rays_edmund = optical_system_edmunds_inverted.propagate_ray(ray_0, propagate_with_first_surface_first=True)
+rays_edmund = optical_system_edmunds.propagate_ray(ray_0, propagate_with_first_surface_first=True)
 
-ax = optical_system_edmunds_inverted.plot()
+ax = optical_system_edmunds.plot()
+ax.scatter([optical_system_edmunds[1].center[0] + 2.68e-3], [0], color='red', label='Back Focal Point')
 rays_edmund.plot(ax=ax)
 ax.set_xlim(-0.02, 0.02)
 plt.show()
